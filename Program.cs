6bd560a0022a04cec4412ad857327fc95c739f6b
David <ladbl1971@gmail.com>
2022-10-11 15:58:04 +0400

    ferst visual posh

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week1_it3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("aa");
+            Console.Title = "mu title";
+            Console.Write("ff");
+            Console.Write("aa");
+            Console.WriteLine("jjjaa");
+            Console.WriteLine("jjkkkjaa");
+            Console.WriteLine("  /|");
+            Console.WriteLine(" / |");
+            Console.WriteLine("/__|");
+        }
+    }
+}
